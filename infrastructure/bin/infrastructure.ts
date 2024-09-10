84b9c936dd32725fe8875b7a058b603b75843aaf
darylduck <daryl@lyraddigital.com>
2024-09-11 01:40:23 +1000

    Trying stack without environment

--- infrastructure/bin/infrastructure.ts
+++ infrastructure/bin/infrastructure.ts
@@ -4,23 +4,7 @@ import * as cdk from "aws-cdk-lib";
 import { InfrastructureStack } from "../lib/infrastructure-stack";
 
 const app = new cdk.App();
-new InfrastructureStack(app, "WordleStack", {
-  env: {
-    account: process.env.CDK_DEFAULT_ACCOUNT,
-    region: process.env.CDK_DEFAULT_REGION,
-  },
-});
 
-new InfrastructureStack(app, "DevelopmentWordleStack", {
-  env: {
-    account: process.env.CDK_DEFAULT_ACCOUNT,
-    region: process.env.CDK_DEFAULT_REGION,
-  },
-});
-
-new InfrastructureStack(app, "TestWordleStack", {
-  env: {
-    account: process.env.CDK_DEFAULT_ACCOUNT,
-    region: process.env.CDK_DEFAULT_REGION,
-  },
-});
+new InfrastructureStack(app, "DevelopmentWordleStack", {});
+new InfrastructureStack(app, "TestWordleStack", {});
+new InfrastructureStack(app, "WordleStack", {});
