bb302ef6361074ba4ae97fa81ba8aee9d4d170d7
darylduck <daryl@lyraddigital.com>
2024-09-13 23:05:56 +1000

    Reverted title

--- frontend-app/app/components/header/header.tsx
+++ frontend-app/app/components/header/header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
     return (
         <header className="flex justify-center w-full border-b-2 border-b-neutral-600 max-w-[500px] mx-6 px-2">
-            <h1 className="text-4xl font-bold uppercase dark:text-white text-center my-4">Wordle 2</h1>
+            <h1 className="text-4xl font-bold uppercase dark:text-white text-center my-4">Wordle</h1>
         </header>
     );
 }
