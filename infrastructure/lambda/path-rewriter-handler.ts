3c674d09e2d9d35989d7b8943d8dec1a51c466da
darylduck <daryl@lyraddigital.com>
2024-09-22 21:10:17 +1000

    Is it failing because of default export?

--- infrastructure/lambda/path-rewriter-handler.ts
+++ infrastructure/lambda/path-rewriter-handler.ts
@@ -3,7 +3,7 @@ import { CloudFrontFunctionsEvent } from "aws-lambda";
 const hasExtension = /(.+)\.[a-zA-Z0-9]{2,5}$/;
 const hasSlash = /\/$/;
 
-export default function handler(event: CloudFrontFunctionsEvent) {
+export function handler(event: CloudFrontFunctionsEvent) {
   const uri = event.request.uri;
 
   if (uri && !uri.match(hasExtension) && !uri.match(hasSlash)) {
