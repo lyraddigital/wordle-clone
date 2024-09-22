9eb2582b7b45f0a0ffcf6edd8e32655227878cbb
darylduck <daryl@lyraddigital.com>
2024-09-22 13:55:15 +1000

    Revert "Forgot https at front"
    This reverts commit 9118efd826b90ac8472f0ca8cceb35fdb5612108.

--- frontend-app/app/components/adsense/adsense.tsx
+++ frontend-app/app/components/adsense/adsense.tsx
@@ -12,7 +12,7 @@ export default function Adsense({ pId }: AdsenseProps) {
   return (
     <Script
       async
-      src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-${pId}`}
+      src={`pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-${pId}`}
       crossOrigin="anonymous"
       strategy="afterInteractive"
     />
