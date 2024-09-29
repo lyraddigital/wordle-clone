27f8fe2ef239f1a1d038a8cd4baf7ae650495be8
darylduck <daryl@lyraddigital.com>
2024-09-29 14:07:16 +1000

    Added a few more conditions

--- infrastructure/lambda/path-rewriter-handler.js
+++ infrastructure/lambda/path-rewriter-handler.js
@@ -1,14 +1,18 @@
 const hasExtension = /(.+)\.[a-zA-Z0-9]{2,5}$/;
+const isHtmlFile = /[.]html$/;
 
 function handler(event) {
   const uri =
     event && event.request && event.request.uri
-      ? event.request.uri.replace(/^\//, "")
+      ? event.request.uri.toLowerCase().replace(/^\//, "")
       : "";
 
   if (uri && uri !== "/") {
     if (!uri.match(hasExtension)) {
       event.request.uri = `/${uri.replace(/\/$/, "")}.html`;
+    } else if (uri.match(isHtmlFile)) {
+      // return a 404. We don't want .html files being returned explicitly
+      event.request.uri = "/404.html";
     }
   } else if (event.request) {
     event.request.uri = "/index.html";
