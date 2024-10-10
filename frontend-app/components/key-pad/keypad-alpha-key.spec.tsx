a94bc0b97a49d72f84aa045e9fce2f14b62cce61
darylduck <daryl@lyraddigital.com>
2024-10-10 21:29:51 +1100

    Some keypad related tests

---
+++ frontend-app/components/key-pad/keypad-alpha-key.spec.tsx
@@ -0,0 +1,105 @@
+jest.mock('@/hooks/wordle/use-wordle');
+
+import { render, screen } from '@testing-library/react';
+
+import { WordleState } from '@/contexts/wordle-context';
+import useWordle from "@/hooks/wordle/use-wordle";
+
+import KeyPadAlphaKey from './keypad-alpha-key';
+
+describe('KeyPadAlphaKey', () => {
+    it('loads the correct button with correct text', () => {
+        // Arrange
+        const letter = "a";
+
+        (useWordle as jest.MockedFunction<typeof useWordle>).mockImplementation(() => ({
+            usedKeys: {}
+        }) as WordleState);
+
+        // Action
+        render(<KeyPadAlphaKey letter={letter} />);
+
+        const button = screen.getByRole('button');
+        const buttonText = button?.textContent;
+
+        // Assert
+        expect(button).toBeDefined();
+        expect(buttonText).toBe(letter);
+    });
+
+    it('loads the correct button with correct class name if no colour is set for the letter', () => {
+        // Arrange
+        const letter = "a";
+
+        (useWordle as jest.MockedFunction<typeof useWordle>).mockImplementation(() => ({
+            usedKeys: {}
+        }) as WordleState);
+
+        // Action
+        render(<KeyPadAlphaKey letter={letter} />);
+
+        const button = screen.getByRole('button');
+        const buttonClassName = button?.className;
+
+        // Assert
+        expect(button).toBeDefined();
+        expect(buttonClassName).toBe("p-0 flex-1 dark:text-neutral-300 uppercase font-bold h-[58px] rounded-[4px] bg-neutral-300 dark:bg-neutral-500");
+    });
+
+    it('loads the correct button with correct class name if green colour is set for the letter', () => {
+        // Arrange
+        const letter: string = "a";
+
+        (useWordle as jest.MockedFunction<typeof useWordle>).mockImplementation(() => ({
+            usedKeys: { [letter]: "green" }
+        }) as WordleState);
+
+        // Action
+        render(<KeyPadAlphaKey letter={letter} />);
+
+        const button = screen.getByRole('button');
+        const buttonClassName = button?.className;
+
+        // Assert
+        expect(button).toBeDefined();
+        expect(buttonClassName).toBe("p-0 flex-1 dark:text-neutral-300 uppercase font-bold h-[58px] rounded-[4px] text-neutral-100 bg-green-600 text-neutral-100 dark:bg-green-700");
+    });
+
+    it('loads the correct button with correct class name if yellow colour is set for the letter', () => {
+        // Arrange
+        const letter: string = "a";
+
+        (useWordle as jest.MockedFunction<typeof useWordle>).mockImplementation(() => ({
+            usedKeys: { [letter]: "yellow" }
+        }) as WordleState);
+
+        // Action
+        render(<KeyPadAlphaKey letter={letter} />);
+
+        const button = screen.getByRole('button');
+        const buttonClassName = button?.className;
+
+        // Assert
+        expect(button).toBeDefined();
+        expect(buttonClassName).toBe("p-0 flex-1 dark:text-neutral-300 uppercase font-bold h-[58px] rounded-[4px] text-neutral-100 bg-yellow-500 dark:bg-yellow-600");
+    });
+
+    it('loads the correct button with correct class name if grey colour is set for the letter', () => {
+        // Arrange
+        const letter: string = "a";
+
+        (useWordle as jest.MockedFunction<typeof useWordle>).mockImplementation(() => ({
+            usedKeys: { [letter]: "grey" }
+        }) as WordleState);
+
+        // Action
+        render(<KeyPadAlphaKey letter={letter} />);
+
+        const button = screen.getByRole('button');
+        const buttonClassName = button?.className;
+
+        // Assert
+        expect(button).toBeDefined();
+        expect(buttonClassName).toBe("p-0 flex-1 dark:text-neutral-300 uppercase font-bold h-[58px] rounded-[4px] text-neutral-100 bg-neutral-500 dark:bg-neutral-700");
+    });
+});
---
+++ frontend-app/components/key-pad/keypad-key.spec.tsx
@@ -0,0 +1,63 @@
+jest.mock('@/hooks/keyboard/use-keypad-press');
+
+import { render, screen } from '@testing-library/react';
+import user from '@testing-library/user-event';
+import useKeypadPress from "@/hooks/keyboard/use-keypad-press";
+
+import KeyPadKey from './keypad-key';
+
+describe('KeyPadKey', () => {
+    it('loads the correct button with correct classes and children', () => {
+        // Arrange
+        const value = "a";
+        const initialButtonContent = (
+            <span>{value}</span>
+        );
+        const classes = "someClass";
+
+        // Action
+        render((
+            <KeyPadKey classes={classes} value={value}>
+                {initialButtonContent}
+            </KeyPadKey>
+        ));
+
+        const button = screen.getByRole('button');
+        const buttonContent = button?.querySelector('span');
+        const buttonClassName = button?.className;
+        const buttonText = buttonContent?.textContent;
+
+        // Assert
+        expect(button).toBeDefined();
+        expect(buttonContent).toBeDefined();
+        expect(buttonClassName).toBeDefined();
+        expect(buttonText).toBeDefined();
+        expect(buttonClassName).toBe(`p-0 flex-1 dark:text-neutral-300 uppercase font-bold h-[58px] rounded-[4px] ${classes}`);
+        expect(buttonText).toBe(value);
+    });
+
+    it('calls click event handler with value when keypad key is pressed', async () => {
+        // Arrange
+        const value = "a";
+        const initialButtonContent = (
+            <span>{value}</span>
+        );
+        const classes = "someClass";
+        const clickHandler = jest.fn();
+
+        (useKeypadPress as jest.MockedFunction<typeof useKeypadPress>).mockImplementationOnce(() => clickHandler);
+
+        // Action
+        render((
+            <KeyPadKey classes={classes} value={value}>
+                {initialButtonContent}
+            </KeyPadKey>
+        ));
+
+        const button = screen.getByRole('button');
+        await user.click(button);
+
+        // Assert        
+        expect(clickHandler).toHaveBeenCalledWith(value);
+    });
+});
