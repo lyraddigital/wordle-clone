a5db637815c679e6f7ab6ca0198eced90acb1930
alinta-darylduck <Daryl.Duckmanton@alintaenergy.com.au>
2024-10-14 11:39:13 +1100

    hook test

---
+++ frontend-app/hooks/keyboard/use-alphabet-keyboard.spec.ts
@@ -0,0 +1,69 @@
+jest.mock("react-hotkeys-hook");
+jest.mock("@/hooks/bindings/use-alphabet-binding");
+
+import { useHotkeys } from "react-hotkeys-hook";
+
+import useAlphabetBinding from "@/hooks/bindings/use-alphabet-binding";
+
+import useAlphabetKeyboard from "./use-alphabet-keyboard";
+
+describe("useAlphabetKeyboard", () => {
+  it("useHostKeys is called with all the letters of the alphabet", () => {
+    // Arrange / Action
+    useAlphabetKeyboard();
+
+    // Assert
+    expect(useHotkeys).toHaveBeenCalled();
+
+    const lettersArray = (useHotkeys as jest.MockedFunction<typeof useHotkeys>)
+      .mock.calls[0][0];
+
+    expect(lettersArray).toStrictEqual([
+      "a",
+      "b",
+      "c",
+      "d",
+      "e",
+      "f",
+      "g",
+      "h",
+      "i",
+      "j",
+      "k",
+      "l",
+      "m",
+      "n",
+      "o",
+      "p",
+      "q",
+      "r",
+      "s",
+      "t",
+      "u",
+      "v",
+      "w",
+      "x",
+      "y",
+      "z",
+    ]);
+  });
+
+  it("useHostKeys is called with all the letters of the alphabet", () => {
+    // Arrange
+    const useAlphabetBindingFn = jest.fn();
+    (
+      useAlphabetBinding as jest.MockedFunction<typeof useAlphabetBinding>
+    ).mockImplementationOnce(useAlphabetBindingFn);
+
+    useAlphabetKeyboard();
+
+    const callback = (useHotkeys as jest.MockedFunction<typeof useHotkeys>).mock
+      .calls[0][1];
+
+    // Action
+    callback({} as KeyboardEvent, {});
+
+    // Assert
+    expect(useAlphabetBindingFn).not.toHaveBeenCalled();
+  });
+});
